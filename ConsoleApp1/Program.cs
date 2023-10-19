ccfebd89c3b294a7b4cfcfc76c4159c0305fd7c0
Zenduro <Zenduro18@gmail.com>
2023-10-19 14:28:14 +0200

    iugguigyuikgyfil

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+Tamagotchi tama = new Tamagotchi();
+
+
+Console.WriteLine("Välkommen till Tamagotchi"); 
+Tamagotchi PlayersTama = new Tamagotchi(); 
+
+Console.WriteLine("Var snäll och skriv din Tamagotchi namn"); 
+PlayersTama.name = Console.ReadLine();  
+
+Console.WriteLine($"Super. {PlayersTama.name} Är ett bra namn för din Tamagotchi!");
+
+while (PlayersTama.GetAlive() ==true)
+{
+  Console.Clear();
+  PlayersTama.PrintStats();
+  Console.WriteLine("Tjooo, vad är de du vill göra?");
+  
+  Console.WriteLine($"1. Lär {PlayersTama.name} Ett nytt ord");
+ 
+  Console.WriteLine($"2.  mata {PlayersTama.name}");
+  
+  Console.WriteLine($"3. Prata med {PlayersTama.name}");
+  
+  Console.WriteLine($"4. ingenting");
+}
+{
+ string doWhat = Console.ReadLine();
+  if (doWhat == "1")
+  {
+    Console.WriteLine("Vad för ord?");
+    string word = Console.ReadLine();
+    PlayersTama.Teach(word);
+  }
+  if (doWhat == "2")
+  {
+    PlayersTama.Feed();
+  }
+  if (doWhat == "3")
+  {
+    PlayersTama.Hi();
+  }
+  else
+  {
+    Console.WriteLine("Gör ingenting");
+  }
+  PlayersTama.Tick();
+  
+}
+
+Console.WriteLine($"Oj Verkar som att din {PlayersTama.name} Är DÖD!");
+Console.ReadLine();
+
+
+
+
---
+++ ConsoleApp1/Tamagotchi.cs
@@ -0,0 +1,78 @@
+﻿
+public class Tamagotchi
+{
+    private int hunger = 0;
+    private int boredom = 0;
+    private List<string> words = new List<string>() {"hej"};
+    private bool isAlive;
+    private Random generator;
+    public string name;
+
+    public Tamagotchi()
+    {
+        isAlive = true;
+        generator = new Random();
+    }
+
+    public void Feed()
+    {
+        Console.WriteLine($" [{name}]Äter och blir mindre hungrig");
+        hunger -= 2;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
+    }
+
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"Boredom: {this.boredom}");
+        Console.WriteLine($"Hunger: {this.hunger}");
+
+        if (isAlive == true)
+        {
+            Console.WriteLine($"You are Alive");
+        }
+        else
+        {
+            Console.WriteLine($"You are not so Alive");
+        }
+
+    }
+    public void Hi()
+    {
+     int wordNum = generator.Next(words.Count); 
+     Console.WriteLine($" [{name}] säger: {words[wordNum]}");
+     ReduceBordom(); 
+    }
+
+    public void Teach(string word)
+    {
+        Console.WriteLine($" [{name}] lär sig: {word}");
+        words.Add(word);
+        ReduceBordom();
+    }
+    public void Tick()
+    {
+        hunger++;
+        boredom++;
+        if (hunger > 10 || boredom > 10)
+        {
+            isAlive = false;
+        }
+    }
+    public bool GetAlive()
+    {
+        return true;
+    }
+    private void ReduceBordom()
+    {
+        Console.WriteLine($" [{name}] Är nu mindre utråkad");
+        boredom -= 2;
+        if (boredom < 0)
+        {
+            boredom = 0;
+        }
+    }
+}
